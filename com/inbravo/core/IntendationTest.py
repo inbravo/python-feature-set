1c1e2736df6a1683cbf23934f5577ea4e8fdfb62
Amit Dixit <inbravo@users.noreply.github.com>
2025-02-26 06:26:53 +0000

    Few more test programs

---
+++ com/inbravo/core/IntendationTest.py
@@ -0,0 +1,5 @@
+if (5 > 3):
+    print("true")
+
+    # This line will throw error as Intendentation is important in Python coding
+     # print("true")
---
+++ com/inbravo/core/VariableTest.py
@@ -0,0 +1,8 @@
+# Python has no command for declaring a variable type
+x = 4       # x is of type int
+y = "inbravo" # y is now of type str
+print(x)
+print(y)
+
+x = "inbravo" # x is now of type str
+print(x)
